a68c2d6c1861ccfebe87a970ee0791ba1f73898d
bluerhino <nie_kl@126.com>
2018-12-03 17:38:18 +0800

    new dgraph branch

---
+++ dgraphwriter/src/main/java/com/alibaba/datax/plugin/writer/mongodbwriter/DgraphWriter.java
@@ -0,0 +1,7 @@
+package com.alibaba.datax.plugin.writer.mongodbwriter;
+
+import com.alibaba.datax.common.spi.Writer;
+
+public class DgraphWriter extends Writer {
+
+}
---
+++ dgraphwriter/src/main/java/com/alibaba/datax/plugin/writer/mongodbwriter/DgraphWriterErrorCode.java
@@ -0,0 +1,31 @@
+package com.alibaba.datax.plugin.writer.mongodbwriter;
+
+import com.alibaba.datax.common.spi.ErrorCode;
+
+/**
+ * @author niekl
+ */
+
+public enum DgraphWriterErrorCode implements ErrorCode {
+
+    UNEXCEPT_EXCEPTION("UNEXCEPT_EXCEPTION","未知异常");
+
+    private final String code;
+
+    private final String description;
+
+    private DgraphWriterErrorCode(String code,String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+}
